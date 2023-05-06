227c42cee3219b8c44ea0e853167c3f21c323f85
Mårten Ward <Marten.ward@gmail.com>
2023-05-06 15:10:43 +0200

    fix(rust): added rust lsp and treesitter

--- lua/plugins/configs/nvimlspconfig.lua
+++ lua/plugins/configs/nvimlspconfig.lua
@@ -1,13 +1,14 @@
 local options = {
-	lua_ls = {},
-	yamlls = {
-		settings = {
-			yaml = {
-				keyOrdering = false,
-			},
-		},
-	},
-	jedi_language_server = {},
-	clangd = {},
+  lua_ls = {},
+  yamlls = {
+    settings = {
+      yaml = {
+        keyOrdering = false,
+      },
+    },
+  },
+  jedi_language_server = {},
+  clangd = {},
+  rust_analyzer = {},
 }
 return options
--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,10 +1,10 @@
 local options = {
-	ensure_installed = { "c", "lua", "vim", "python", "yaml", "terraform" },
-	sync_install = false,
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = { enable = true },
+  ensure_installed = { "c", "lua", "vim", "python", "yaml", "terraform", "rust" },
+  sync_install = false,
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  indent = { enable = true },
 }
 return options
