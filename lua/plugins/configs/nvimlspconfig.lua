81b33cc0dbee5f3b515cee6eda21c5ce82f452a7
Marten Ward <marten.ward@gmail.com>
2024-08-21 19:15:04 +0200

    feat: removed that clicked j too many times

--- lua/plugins/configs/nvimlspconfig.lua
+++ lua/plugins/configs/nvimlspconfig.lua
@@ -1,16 +1,18 @@
 local options = { -- Use command :LspLog to debug lsps, remember to update init.lua and install lsp
-	lua_ls = {},
-	yamlls = {
-		settings = {
-			yaml = {
-				keyOrdering = false,
-			},
-		},
-	},
-	jedi_language_server = {},
-	jdtls = {},
-	clangd = {},
-	rust_analyzer = {},
-	gopls = {},
+  lua_ls = {},
+  yamlls = {
+    settings = {
+      yaml = {
+        keyOrdering = false,
+      },
+    },
+  },
+  jedi_language_server = {},
+  jdtls = {},
+  clangd = {
+    offset_encoding = 'utf-8'
+  },
+  rust_analyzer = {},
+  gopls = {},
 }
 return options
